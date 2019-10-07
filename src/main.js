6d7f7b1745b1a169db3f29d1e9ff70c17053c0d7
gunturbudi16 <gunturkurniawan411@yahoo.co.id>
2019-10-07 15:38:51 +0700

    haha

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import { connect } from "react-redux";
-// import { Carousel } from "react-bootstrap";
-// import image1 from "./assets/img/1.jpg";
-// import image2 from "./assets/img/2.jpg";
-// import image3 from "./assets/img/3.jpg";
+import Carousel from "react-bootstrap/Carousel";
+import image1 from "./assets/img/1.jpg";
+import image2 from "./assets/img/2.jpg";
+import image3 from "./assets/img/3.jpg";
 class Homepage extends Component {
   render() {
     return (
@@ -20,29 +20,33 @@ class Homepage extends Component {
             Learn React{this.props.testredux}
           </a>
         </header>
-        {/* <Carousel>
-          <Carousel.item>
-            <img className="d-block w-100" src={image1} alt="" />
-            <Carousel.caption>
-              <h3>belajarjajajja</h3>
-              <p>hbhkkhbkb</p>
-            </Carousel.caption>
-          </Carousel.item>
-          <Carousel.item>
-            <img className="d-block w-100" src={image2} alt="" />
-            <Carousel.caption>
-              <h3>Belajar</h3>
-              <p>hahahahah</p>
-            </Carousel.caption>
-          </Carousel.item>
-          <Carousel.item>
-            <img className="d-block w-100" src={image3} alt="" />
-            <Carousel.caption>
-              <h3>Belajar Donor dari sekarang</h3>
-              <p>hahahhaha</p>
-            </Carousel.caption>
-          </Carousel.item>
-        </Carousel> */}
+        <Carousel>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image1} alt="First slide" />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image2} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={image3} alt="Third slide" />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
       </div>
     );
   }
