4ca8a5f5511e235032eb0ad84f2b74a9d4c6691d
gunturbudi16 <gunturkurniawan411@yahoo.co.id>
2019-10-08 23:29:29 +0700

    last

---
+++ src/components/AppLoading/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HalamanUtama() {
+  return (
+    <div>
+      <h1>halaman utama</h1>
+    </div>
+  );
+}
--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
 export default function PrivateRoute({ component: Component, ...restProps }) {
-  const user = true;
+  const user = false;
   return (
     <Route
       {...restProps}
