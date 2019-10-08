3f8608c76deb33001b068b3b1b127fb8c97eaf38
GAlangHarindito <galang.harindito@gmail.com>
2019-10-08 14:21:34 +0700

    handle sign in

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
-import axios from "axios"; 
+import axios from "axios";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 
 export default function SignIn(props) {
-  const [value, setValue] = useState({ prm_akun: "", passuser: "" });
+  const [value, setValue] = useState({ prm_akun: "", pass_user: "" });
 
   function handleSubmit(event) {
-    event.preventDefault(props)
+    event.preventDefault();
     axios
-    .post(`https://my-mysql-api.herokuapp.com/user/login`, value, {
-      headers: {
-        "Access-Control-Allow-Origin": '*'
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    });
+      .post(`https://my-mysql-api.herokuapp.com/user/login`, value, {
+        headers: {
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(result => alert(result.data.message))
+      .catch(error => {
+        console.log(error);
+      });
   }
-  
 
   function handleChange(event) {
     setValue({
@@ -30,15 +30,25 @@ export default function SignIn(props) {
     <Form onSubmit={handleSubmit}>
       <FormGroup>
         <Label for="prm_akun">Name / Email</Label>
-        <Input type="text" name="prm_akun" id="prm_akun" placeholder="" onChange={handleChange}  />
+        <Input
+          type="text"
+          name="prm_akun"
+          placeholder=""
+          onChange={handleChange}
+        />
       </FormGroup>
       <FormGroup>
         <Label for="password">Password</Label>
-        <Input type="password" name="passuser" id="passuser" placeholder="" onChange={handleChange} />
+        <Input
+          type="password"
+          name="pass_user"
+          placeholder=""
+          onChange={handleChange}
+        />
       </FormGroup>
-      <Button className="btn-block" onClick={handleSubmit} >Submit</Button>
+      <Button className="btn-block" onClick={handleSubmit}>
+        Submit
+      </Button>
     </Form>
   );
 }
-
-
